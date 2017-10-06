b6f68a10a6ce4fd4d844c16a1305741f9c92ff0a
Thien Le <thienle.2401@gmail.com>
2017-10-06 23:00:00 +0100

    Add -y to apt-get commands and apt-get update before installing docker-ce

--- config/config-01-InstallDocker.sh
+++ config/config-01-InstallDocker.sh
@@ -2,28 +2,30 @@
 
 # https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#recommended-extra-packages-for-trusty-1404
 
-echo "install Docker"
+echo "--------------------------------------------"
+echo "Installing Docker --------------------------"
 
 # install the linux-image-extra-* packages
-sudo apt-get install \
+sudo apt-get install -y\
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual
 
 # install packages to allow apt to use a repository over HTTPS:
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 	
-# add Docker’s official GPG ke
+# add Dockers official GPG ke
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -	
 
 # set up the stable repository
-sudo add-apt-repository \
+sudo add-apt-repository -y \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+sudo apt-get update
 
 # install Docker CE
 sudo apt-get install docker-ce
