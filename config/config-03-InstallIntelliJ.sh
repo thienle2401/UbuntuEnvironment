f5326c5347bf3d6724ae6f378dbc99e753039d63
Thien Le <thienle.2401@gmail.com>
2017-10-22 14:44:38 +0100

    Added rm for IntelliJ and PyCharm.

--- config/config-03-InstallIntelliJ.sh
+++ config/config-03-InstallIntelliJ.sh
@@ -5,5 +5,6 @@ echo "Installing InteliJ IDEA --------------------"
 
 wget -O /tmp/ideaIC-2017.2.5.tar.gz --no-proxy http://download.jetbrains.com/idea/ideaIC-2017.2.5.tar.gz
 sudo tar xfz /tmp/ideaIC-2017.2.5.tar.gz -C /opt/
+rm /tmp/ideaIC-2017.2.5.tar.gz
 
 echo "--------------------------------------------"
--- config/config-07-InstallPyCharm.sh
+++ config/config-07-InstallPyCharm.sh
@@ -5,5 +5,6 @@ echo "Installing PyCharm Community ---------------"
 
 wget -O /tmp/pycharm-community-2017.2.3.tar.gz --no-proxy https://download.jetbrains.com/python/pycharm-community-2017.2.3.tar.gz
 sudo tar xfz /tmp/pycharm-community-2017.2.3.tar.gz -C /opt/
+rm /tmp/pycharm-community-2017.2.3.tar.gz
 
 echo "--------------------------------------------"
