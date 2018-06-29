e7a4e8b0668d6ed3c8e4dd6da7fd35812ccdb946
Thien Le <thienle.2401@gmail.com>
2018-06-29 23:27:50 +0100

    Updated installation for r-base-core and README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Setup Bare Ubuntu
-Install some basic stuffs for bare Ubuntu.
+Install some basic stuffs for s bare Ubuntu installation.
+
+version: 18.04
--- config/config-05-RStudio.sh
+++ config/config-05-RStudio.sh
@@ -3,7 +3,7 @@
 echo "install R and RStudio"
 
 # install RStudio
-sudo apt-get install -Y r-base
+sudo apt-get install -Y r-base-core
 sudo apt-get install gdebi-core
 wget -O /tmp/rstudio-xenial-1.0.153-amd64.deb --no-proxy https://download1.rstudio.org/rstudio-xenial-1.0.153-amd64.deb
 sudo gdebi -n /tmp/rstudio-xenial-1.0.153-amd64.deb
