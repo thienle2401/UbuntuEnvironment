bba1bf298ee6258596c220253175b86d5dca8c0a
Thien Le <thienle.2401@gmail.com>
2018-06-30 13:28:55 +0100

    Updated RStudio to "v1.2.747-3 Preview".

--- config/config-05-RStudio.sh
+++ config/config-05-RStudio.sh
@@ -5,6 +5,6 @@ echo "install R and RStudio"
 # install RStudio
 sudo apt-get install -Y r-base-core
 sudo apt-get install gdebi-core
-wget -O /tmp/rstudio-xenial-1.0.153-amd64.deb --no-proxy https://download1.rstudio.org/rstudio-xenial-1.0.153-amd64.deb
-sudo gdebi -n /tmp/rstudio-xenial-1.0.153-amd64.deb
-rm /tmp/rstudio-xenial-1.0.153-amd64.deb
+wget -O /tmp/rstudio-1.2.747-amd64.deb --no-proxy https://s3.amazonaws.com/rstudio-ide-build/desktop/trusty/amd64/rstudio-1.2.747-amd64.deb
+sudo gdebi -n /tmp/rstudio-1.2.747-amd64.deb
+rm /tmp/rstudio-1.2.747-amd64.deb
